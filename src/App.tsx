95ea3f3393fb185d11d7a25a345c73819ebc83d7
George Tsendra <george.tsendra66@gmail.com>
2024-11-15 19:23:40 +0100

    [+] - basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import { useEffect } from "react";
-
+import { Routes, Route } from "react-router-dom";
 import useLocalizeDocumentAttributes from "./i18n/useLocalizeDocumentAttributes";
 import "./i18n/config";
 
-import Sidebar from "./components/Layout/Sidebar/Sidebar";
-import Header from "./components/Layout/Header/Header";
-import TableView from "./components/Layout/TableView/TableView";
-import TableManagementSection from "./components/Layout/TableManagementSection/TableManagementSection";
+import Dashboard from "./page/Dashboard";
 
 function App() {
   useLocalizeDocumentAttributes();
 
   return (
-    <main className="flex flex-row h-[100vh]">
-      <Sidebar />
-      <div className="flex flex-col flex-1 p-8 pt-10">
-        <Header />
-        <div className="mb-10 mt-10">
-          <TableManagementSection />
-        </div>
-        <TableView />
-      </div>
-    </main>
+    <>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
